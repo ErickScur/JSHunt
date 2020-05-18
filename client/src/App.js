fc5ca0cd0c6325f440adc3a166192786f434b1d2
Erick Scur <erickscur@gmail.com>
2020-05-18 12:10:10 -0300

    Importing footer into app

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Header from './components/Header'
 import './styles.css';
 import Main from './pages/main'
 import Routes from './routes'
+import Footer from './components/Footer'
 const App = () => (
   <div className="App">
     <Header />
     <Routes />
+    <Footer />
   </div>
 );
 
